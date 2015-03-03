99a6a8ae6c2979529f03f29b2e7d78afedc67b39
Pete Arden <pete.davidarden@gmail.com>
2015-03-03 00:24:59 +0000

    Update quadraticequationsolver.php

--- quadraticequationsolver.php
+++ quadraticequationsolver.php
@@ -6,6 +6,7 @@
 </head>
 
 <body>
+
 <?php
 $a = $_GET["a"];
 $b = $_GET["b"];
@@ -13,28 +14,53 @@ $c = $_GET["c"];
 
 $discriminant = (($b * $b) - (4 * $a * $c));
 
+if ($a == null || $b == null || $c == null)
+{
+  echo "<h2>Code/QuadraticEquationSolver:</h2>";
+	echo "Something went wrong... Make sure you enter numbers...";
+	echo "<html><br><br></html>";
+
+}
+
+else
+
+{
+
 if ($discriminant < 0)
 	{
+	echo "<h2>Code/QuadraticEquationSolver:</h2>";
 	echo "Imaginary solutions are not possible yet...";
 	echo "<html><br><br></html>";
-	}	
+	}
+	
 
 else if ($discriminant > 0)
 {
 $x1 = ((-$b + (sqrt(($b * $b) - (4 * $a * $c))))/(2*$a));
 $x2 = ((-$b - (sqrt(($b * $b) - (4 * $a * $c))))/(2*$a));
 
-echo "Here are the solutions to the equation:";
-echo "<html><br><br></html>";
+echo "<h2>Code/QuadraticEquationSolver: Here are the solutions to the equation</h2>";
 echo "x1 = ", $x1;
-echo "<html><br></html>";
+echo "<html><br><br></html>";
 echo "x2 = ", $x2;
 }
 
+else if ($discriminant == 0)
+{
+$x = ((-$b + (sqrt(($b * $b) - (4 * $a * $c))))/(2*$a));
+echo "<h2>Code/QuadraticEquationSolver: The equation has one solution</h2>";
+echo "x = ", $x;
+echo "<html><br><br></html>";
+}
+
 else {
+	echo "<h2>Code/QuadraticEquationSolver:</h2>";
 	echo "Something went wrong... Make sure you enter numbers...";
 	echo "<html><br><br></html>";
 	}
-?> 
+}
+
+?>
+
 </body>
 </html>
